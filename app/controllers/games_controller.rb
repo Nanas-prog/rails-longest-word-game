a49383c57d7a2223b8892bd831f1b58cc1f2c3a1
anais roth <rothanais@yahoo.fr>
2019-11-12 11:10:01 +0100

    ex 3

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,14 @@
+class GamesController < ApplicationController
+  def new
+    @letters = ('A'..'Z').to_a.sample(10)
+  end
+
+  def score
+  end
+
+  def english_word?(word)
+  response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+  json = JSON.parse(response.read)
+  return json['found']
+  end
+end
